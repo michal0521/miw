6a9a3f7a2ead8ba493dcb3d6f6194b406975eb1e
Michał Gębicki <gebicki.michal98@gmail.com>
2020-05-09 13:22:46 +0200

    inheritance - calculator

--- 02/cw4.py
+++ 02/cw4.py
@@ -1,3 +1,30 @@
 class Calculator:
-    def add(self, a, b):
+    @staticmethod
+    def add(a, b):
         return a + b
+    
+    @staticmethod
+    def difference(a, b):
+        return a - b
+    
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+    
+    @staticmethod
+    def divide(a, b):
+        if b == 0:
+            raise Exception('Cannot divide by 0')
+        
+        result = a / b
+        
+        return '{:.4f}'.format(result)
+    
+class ScientificCalculator(Calculator): 
+    @staticmethod
+    def power(a, power):
+        return a**power
+    
+print(ScientificCalculator.add(5, 2))
+
+    
